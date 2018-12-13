61c5ca13040b57b21044e796946c88133c11d6f7
dityara17 <arizki717@gmail.com>
2018-12-13 20:38:08 +0700

    update return response

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,25 +15,28 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
 
-    public function getPosts(){
-        return Post::orderBy('id','desc')->get();
+    public function getPosts()
+    {
+        return Post::orderBy('id', 'desc')->get();
     }
 
-    public function getPost($id){
+    public function getPost($id)
+    {
         return Post::findOrFail($id);
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $post = new Post();
         $post->category_id = $request->category;
         $post->name = $request->name;
         //image insert
-        if (!empty($request->image)){
+        if (!empty($request->image)) {
             if ($request->hasFile('image')) {
                 $image = $request->file('image');
                 $fullName = $image->getClientOriginalName();
-                $n = explode('.',$fullName)[0];
-                $name = $n."-".time().'.'.$image->getClientOriginalExtension();
+                $n = explode('.', $fullName)[0];
+                $name = $n . "-" . time() . '.' . $image->getClientOriginalExtension();
                 $destinationPath = storage_path('app/images/');
                 $image->move($destinationPath, $name);
                 $post->image = $name;
@@ -43,21 +46,26 @@ class PostController extends Controller
         $post->author = $request->author;
         $post->save();
         return response()->json([
-           'success' => true
+            'success' => true,
+            'name' => $request->name,
+            'category_id' => $request->category,
+            'desc' => $request->description,
+            'author' => $request->author
         ]);
     }
 
-    public function update(Request $request,$id){
+    public function update(Request $request, $id)
+    {
         $post = Post::findOrFail($id);
         $post->category_id = $request->category;
         $post->name = $request->name;
         //image insert
-        if (!empty($request->image)){
+        if (!empty($request->image)) {
             if ($request->hasFile('image')) {
                 $image = $request->file('image');
                 $fullName = $image->getClientOriginalName();
-                $n = explode('.',$fullName)[0];
-                $name = $n."-".time().'.'.$image->getClientOriginalExtension();
+                $n = explode('.', $fullName)[0];
+                $name = $n . "-" . time() . '.' . $image->getClientOriginalExtension();
                 $destinationPath = storage_path('app/images/');
                 $image->move($destinationPath, $name);
                 $post->image = $name;
@@ -67,11 +75,16 @@ class PostController extends Controller
         $post->author = $request->author;
         $post->update();
         return response()->json([
-            'success' => true
+            'success' => true,
+            'name' => $post->name,
+            'category_id' => $post->category_id,
+            'desc' => $post->desc,
+            'author' => $post->author
         ]);
     }
 
-    public function destoryPosts($id){
+    public function destoryPosts($id)
+    {
         $post = Post::findOrFail($id);
         $post->delete();
         return response()->json([
